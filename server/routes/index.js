9361ec0c02caa57601467f81799d0f5546bbe9e7
igormjq <igormjq@gmail.com>
2017-12-06 23:53:06 -0200

    /

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const router = require('express').Router();
 const todosRoutes = require('./todo');
 
+router.get('/', (req, res) => {
+    res.json({
+        details: 'Todo API',
+        version: 1
+    });
+});
 router.use('/todos', todosRoutes)
 
 module.exports = router;
