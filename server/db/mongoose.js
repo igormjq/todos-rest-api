e1706ba79f6f6c8da2585542137144d9d512780d
igormjq <igormjq@gmail.com>
2017-12-07 00:09:59 -0200

    mlabs uri added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 let mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+mongoose.connect(uri, { useMongoClient: true });
 
 module.exports = { mongoose };
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const router = require('express').Router();
 const todosRoutes = require('./todo');
 
+router.use('/todos', todosRoutes);
 router.get('/', (req, res) => {
     res.json({
         details: 'Todo API',
         version: 1
     });
 });
-router.use('/todos', todosRoutes)
+
 
 module.exports = router;
